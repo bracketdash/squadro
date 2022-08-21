32c8015b2c5c14ca6d804e81b6c393ef4efe3d9f
Michael Hatch <michaelhatch@me.com>
2022-08-20 17:06:23 -0700

    slapped together a ui for quicker testing

--- src/Squadro.js
+++ src/Squadro.js
@@ -1,6 +1,8 @@
-const MAX_MOVES_AHEAD = 2;
+import { useState } from 'react';
 
-const getMoveScores = ({ player, state }) => {
+const MAX_MOVES_AHEAD = 0;
+
+const getMoveScores = ({ player, state }) => new Promise((resolve) => {
   const si = player === 1 ? 0 : 5;
   const options = [];
   Array(5).fill(true).forEach((_, i) => {
@@ -12,9 +14,9 @@ const getMoveScores = ({ player, state }) => {
     }
   });
   Promise.all(options).then((values) => {
-    console.log(values);
+    resolve(values);
   });
-};
+});
 
 const getOpponentPiece = ({ piece, space }) => {
   let spaceMap;
@@ -127,8 +129,37 @@ getMoveScores({
 /////
 
 const Squadro = () => {
+  const [uState, setUState] = useState(Array(10).fill(0).join(','));
+  const [uPlayer, setUPlayer] = useState(1);
+  const [uScores, setUScores] = useState([0, 0, 0, 0, 0]);
   return (
     <div className="container">
+      <p>
+        This is bare bones right now.
+        Enter the game state and which player you are below.
+        Game state is a 10-element array, representing the 10 pieces, with values 0 - 12 representing their position on their track.
+        For player, enter 1 if your home row is the clockwise-most starting side.
+      </p>
+      <p>
+        <label>Game state (commas-separated, no brackets or spaces):</label><br />
+        <input type="text" defaultValue={uState} onKeyUp={(e) => setUState(e.target.value)} />
+      </p>
+      <p>
+        <label>Player:</label><br />
+        <input type="text" defaultValue={uPlayer} onKeyUp={(e) => setUPlayer(e.target.value)} />
+      </p>
+      <button onClick={() => {
+        getMoveScores({
+          player: uPlayer,
+          state: uState.split(','),
+        }).then((scores) => {
+          setUScores(scores.map((s) => s.value));
+        });
+      }}>Get moves scores</button>
+      <p>
+        <label>Moves scores:</label>
+      </p>
+      <pre>{uScores.join(' | ')}</pre>
     </div>
   );
 }
